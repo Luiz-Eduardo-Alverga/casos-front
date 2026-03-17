b808ad0991139032aaa755690ce35c731d7396ac
luiz <luizzz154@hotmail.com>
2026-03-17 14:16:39 -0300

    permitir textarea da descricao completa ser redimensionavel

--- components/caso-form/fields/caso-form-descricao-completa.tsx
+++ components/caso-form/fields/caso-form-descricao-completa.tsx
@@ -7,23 +7,31 @@ import { useFormContext } from "react-hook-form";
 
 export function CasoFormDescricaoCompleta() {
   const { isDisabled } = useCasoForm();
-  const { register, formState: { errors } } = useFormContext();
-  
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
   return (
     <div className="space-y-2">
       <div className="flex justify-between">
-        <Label htmlFor="description" className="text-sm font-medium text-text-label">
+        <Label
+          htmlFor="description"
+          className="text-sm font-medium text-text-label"
+        >
           Descrição Completa <span className="text-text-error">*</span>
         </Label>
 
         {errors.DescricaoCompleta && (
-          <p className="text-sm text-destructive">{errors.DescricaoCompleta.message as string}</p>
+          <p className="text-sm text-destructive">
+            {errors.DescricaoCompleta.message as string}
+          </p>
         )}
       </div>
       <Textarea
         id="description"
         placeholder="Descreva detalhadamente o caso, incluindo contexto, passos para reproduzir e comportamento esperado..."
-        className="min-h-[158px] resize-none rounded-lg border-border-input px-[17px] py-[13px]"
+        className="min-h-[158px]  rounded-lg border-border-input px-[17px] py-[13px]"
         {...register("DescricaoCompleta")}
         disabled={isDisabled}
       />
